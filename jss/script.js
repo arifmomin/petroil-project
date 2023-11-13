6f7de8fa611dcb39163284109c038430c20447c8
Arif Momin <148381072+arifmomin@users.noreply.github.com>
2023-11-13 22:29:15 +0600

    Add files via upload

---
+++ jss/script.js
@@ -0,0 +1,32 @@
+// ===============back to top button==================
+let back_to_top = document.querySelector('.back_to_top')
+console.log(back_to_top)
+window.addEventListener('scroll', function () {
+  let scrolling = this.scrollY
+  if (scrolling > 100) {
+    back_to_top.classList.add('go_top')
+  } else {
+    back_to_top.classList.remove('go_top')
+  }
+})
+
+back_to_top.addEventListener('click',function(){
+  window.scrollTo({
+    top:0,
+    behavior:"smooth",
+  })
+
+})
+// ----------------back to top button-----------------
+// ======================= menu part==============
+let menu_bar = document.querySelector('.menu_bar')
+console.log(menu_bar)
+window.addEventListener('scroll', function () {
+  let scrolling = this.scrollY
+  if (scrolling > 100) {
+    menu_bar.classList.add('menu_fixed')
+  } else {
+    menu_bar.classList.remove('menu_fixed')
+  }
+})
+// ==================menu fixed======================
